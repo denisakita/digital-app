7f2f250ea2e590fab9d9e5a3e2e21224e729c419
Denisa Kita <denisakita7@gmail.com>
2023-11-16 23:20:21 +0100

    add components in app

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CartDeal from "./CartDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+
+export {
+    Navbar,
+    Billing,
+    Business,
+    Button,
+    CartDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials
+}
