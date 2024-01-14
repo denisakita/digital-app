cd3a704387dcbdcff8284750dab637fcf9bfaf43
Denisa Kita <denisakita7@gmail.com>
2024-01-14 17:25:07 +0100

    work with hero js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./style";
-import {Billing, Business, CartDeal, Clients, CTA, Footer, Hero, Navbar, Stats, Testimonials} from "./components";
+import {Billing, Business, Clients, CTA, Footer, Hero, Navbar, Stats, Testimonials} from "./components";
 import CardDeal from "./components/CartDeal";
 
 
@@ -8,26 +8,26 @@ const App = () => (
     <div className="bg-primary w-full overflow-hidden">
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
             <div className={`${styles.boxWidth}`}>
-                <Navbar />
+                <Navbar/>
             </div>
         </div>
 
         <div className={`bg-primary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
-                <Hero />
+                <Hero/>
             </div>
         </div>
 
         <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
             <div className={`${styles.boxWidth}`}>
-                <Stats />
-                <Business />
-                <Billing />
-                <CardDeal />
-                <Testimonials />
-                <Clients />
-                <CTA />
-                <Footer />
+                <Stats/>
+                <Business/>
+                <Billing/>
+                <CardDeal/>
+                <Testimonials/>
+                <Clients/>
+                <CTA/>
+                <Footer/>
             </div>
         </div>
     </div>
