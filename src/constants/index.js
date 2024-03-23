7278100d48507697cc38dbbccde04595f9e8dcc6
denisakita <78655648+denisakita@users.noreply.github.com>
2024-03-23 19:44:56 +0100

    changes (#2)

--- src/constants/index.js
+++ src/constants/index.js
@@ -94,23 +94,23 @@ export const footerLinks = [
         links: [
             {
                 name: "Content",
-                link: "https://www.hoobank.com/content/",
+                link: "https://www.usbank.com/content/",
             },
             {
                 name: "How it Works",
-                link: "https://www.hoobank.com/how-it-works/",
+                link: "https://www.usbank.com/how-it-works/",
             },
             {
                 name: "Create",
-                link: "https://www.hoobank.com/create/",
+                link: "https://www.usbank.com/create/",
             },
             {
                 name: "Explore",
-                link: "https://www.hoobank.com/explore/",
+                link: "https://www.usbank.com/explore/",
             },
             {
                 name: "Terms & Services",
-                link: "https://www.hoobank.com/terms-and-services/",
+                link: "https://www.usbank.com/terms-and-services/",
             },
         ],
     },
@@ -119,23 +119,23 @@ export const footerLinks = [
         links: [
             {
                 name: "Help Center",
-                link: "https://www.hoobank.com/help-center/",
+                link: "https://www.usbank.com/help-center/",
             },
             {
                 name: "Partners",
-                link: "https://www.hoobank.com/partners/",
+                link: "https://www.usbank.com/partners/",
             },
             {
                 name: "Suggestions",
-                link: "https://www.hoobank.com/suggestions/",
+                link: "https://www.usbank.com/suggestions/",
             },
             {
                 name: "Blog",
-                link: "https://www.hoobank.com/blog/",
+                link: "https://www.usbank.com/blog/",
             },
             {
                 name: "Newsletters",
-                link: "https://www.hoobank.com/newsletters/",
+                link: "https://www.usbank.com/newsletters/",
             },
         ],
     },
@@ -144,11 +144,11 @@ export const footerLinks = [
         links: [
             {
                 name: "Our Partner",
-                link: "https://www.hoobank.com/our-partner/",
+                link: "https://www.usbank.com/our-partner/",
             },
             {
                 name: "Become a Partner",
-                link: "https://www.hoobank.com/become-a-partner/",
+                link: "https://www.usbank.com/become-a-partner/",
             },
         ],
     },
